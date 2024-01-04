6e3e6cbf9791fcdccf73338d1fba28411ab6c54d
MarcoSilva0 <marcoantonio02016@outlook.com>
2024-01-04 13:35:41 -0300

    feat: Add jest and init

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,10 @@
+describe('Testado a persistencia de dados', () => {
+  it('Test Description (IT)', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+  test('Test Description (TEST)', () => {
+    const nome = 'Marco';
+    expect(nome).toBe('Marco');
+  });
+});
