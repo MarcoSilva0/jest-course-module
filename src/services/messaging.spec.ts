f70cd41aa45f06193d730042af9c6b8123097d5b
MarcoSilva0 <marcoantonio02016@outlook.com>
2024-01-05 11:34:32 -0300

    feat: Add test in Messaging Class

---
+++ src/services/messaging.spec.ts
@@ -0,0 +1,30 @@
+import { Messaging } from './Messaging';
+
+const createSut = () => {
+  return new Messaging();
+};
+
+describe('Messaging', () => {
+  //Clean all mock after suit test
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    //Sytem under testing
+    const sut = createSut();
+    expect(sut.sendMenssage('Teste')).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMenssage('Teste');
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Mensagem Enviada:" and msg', () => {
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMenssage('Teste');
+    expect(consoleSpy).toHaveBeenCalledWith('Mensagem Enviada:', 'Teste');
+  });
+});
